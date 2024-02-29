6c7cf7b5b5c22b619520a8f1b3beccbc570cdf85
Jorge Willian Paez Nagakura <jorge@infoworker.com.br>
2024-02-28 22:36:20 -0300

    Melhorando formulários

--- SmartHint/Models/Cliente.cs
+++ SmartHint/Models/Cliente.cs
@@ -1,17 +1,35 @@
-﻿namespace SmartHint.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartHint.Models
 {
     public class Cliente
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(150, ErrorMessage = "Máximo de 150 caracteres")]
         public string NomeClienteRazaoSocial { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MaxLength(150, ErrorMessage = "Máximo de 150 caracteres")]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
-        public string Telefone { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int Telefone { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string TipoPessoa { get; set; } = string.Empty;
-        public string CPFCNPJ { get; set; } = string.Empty;
-        public string InscricaoEstadual { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int CPFCNPJ { get; set; }
+        public int InscricaoEstadual { get; set; }
         public bool Isento { get; set; } = false;
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Genero { get; set; } = string.Empty;
+        public DateOnly DataNascimento {  get; set; }
         public bool Bloqueado { get; set; } = false;
+        [Required(ErrorMessage = "Campo obrigatório")]
+        //[MinLength(11, ErrorMessage = "Mínimo 8 caracteres")]
+        //[MaxLength(15, ErrorMessage = "Máximo 15 caracteres")]
         public string Senha { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmarSenha { get; set; } = string.Empty;
     }
 }
